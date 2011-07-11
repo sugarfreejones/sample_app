c1ea87a5f5a2f812ea792bc92eafd08a58b50abd
Rob Jones <sugarfreejones@gmail.com>
2011-07-11 17:19:18 -0500

    Added the password machinery

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -104,14 +104,57 @@ describe User do
     describe "password encryption" do
       
       before :each do
-        @user = User.new(@attr)
+        @user = User.create!(@attr)
       end
     
       it "should have an encrypted password attribute" do
         @user.should respond_to(:encrypted_password)
       end
-    end
+      
+      it "should set the encrypted password attribute" do
+        @user.encrypted_password.should_not be_blank
+      end
+      
+      it "should have a salt" do
+        @user.should respond_to(:salt)
+      end
+
+      describe "has_password? method" do 
+        
+        it "should exist" do
+          @user.should respond_to(:has_password?)
+        end
+        
+        it "should return true if the passwords match" do
+          @user.has_password?(@attr[:password]).should be_true
+        end
+
+        it "should return false if the passwords don't match" do
+          @user.has_password?("invalid").should be_false
+        end  
+      end
     
+      describe "authenticate method" do
+        
+        it "should exist" do 
+          User.should respond_to(:authenticate)
+        end
+        
+        it "should return nil on email/pass mismatch" do
+          User.authenticate(@attr[:email], "wrongpass").should be_nil
+        end
+        
+        it "should return nil on email address with no user " do
+          User.authenticate("bar@foo.com", @attr[:password]).should be_nil
+        end
+          
+        it "should return user on email/pass match" do
+          User.authenticate(@attr[:email], @attr[:password]).should == @user
+        end 
+
+      end
+    
+    end  
 end
 
 
