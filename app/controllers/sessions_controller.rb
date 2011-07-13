3f7f6e30a18bf942da8386493e4fb6f0d35c364b
Rob Jones <sugarfreejones@gmail.com>
2011-07-13 14:42:11 -0500

    Handle invalid signing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,13 @@ class SessionsController < ApplicationController
   end
   
   def create
+    user = User.authenticate(params[:session][:email], params[:session][:password])
+    if user.nil?
+        flash.now[:error] = "Invalid email/password combination"
+        render 'new'
+    else
+        # handle successful signin here
+    end
     
   end
   
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -17,4 +17,30 @@ describe SessionsController do
     
   end
 
+  describe "POST 'create'" do
+
+    describe "failure" do
+      
+      before (:each) do 
+        @attr = {:email => "", :password => ""}
+      end
+      
+      it "should re-render the new page" do
+        post :create, :session => @attr 
+      end
+ # for some reason, the title changes when we flash; we lose the Sign in title
+ #     it "should have the right title" do
+ #        post :create, :session => @attr
+ #        response.should have_selector("title", :content => "Sign in")
+ #      end
+
+      it "should have a flash.now message" do
+        post :create, :session => @attr
+        flash.now[:error].should =~ /invalid/i
+      end
+    
+    end
+    
+  end
+
 end
