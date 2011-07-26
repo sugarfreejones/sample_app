e048f2e0fee7aebca4e0938f306dba67e29ebae4
Rob Jones <sugarfreejones@gmail.com>
2011-07-26 10:28:00 -0500

    Added follower/following pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   
-  before_filter :authenticate , :only => [:index,:edit,:update, :destroy]
+  before_filter :authenticate , :except => [:show, :new, :create]
   before_filter :correct_user , :only => [:edit, :update]
   before_filter :admin_user, :only => :destroy
   
@@ -16,6 +16,26 @@ class UsersController < ApplicationController
 #    @microposts = @user.microposts.paginate(:page => params[:page])
     @microposts = @user.microposts
     @title = @user.name
+  end
+  
+
+  def following 
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following
+    # if pagination worked, use this 
+    # @users = @user.following.paginate(:page => params[:page])
+    render 'show_follow'
+  end
+  
+  
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers
+    # if pagination worked, use this 
+    # @users = @user.followers.paginate(:page => params[:page])
+    render 'show_follow'    
   end
   
   def new
